89ce155e7ebe03545b3da0fc3093556cb68ae5f2
Zhu Liliang <liliang.zhu@gmail.com>
2017-06-21 13:55:36 +0800

    update swagger path

--- swagger/build-swagger.sh
+++ swagger/build-swagger.sh
@@ -1,5 +1,6 @@
 curl -o proposal.json http://106.14.50.232/proposal/v2/api-docs?group=proposals;
-curl -u life:eBao123 -o product.json http://106.14.50.232/pd/rest/v2/li-api-docs?group=products;
+#curl -u life:eBao123 -o product.json http://106.14.50.232/pd/rest/v2/li-api-docs?group=products;
+curl -o product.json http://106.14.50.232/pd/v2/li-api-docs?group=products;
 
 node merge.js
 
