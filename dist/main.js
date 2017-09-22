d6fcc2925d6f3761f57879602e160fc0f9ba4082
Zhu Liliang <liliang.zhu@gmail.com>
2017-09-22 17:45:37 +0800

    update js error to fix chrome issue

--- dist/main.js
+++ dist/main.js
@@ -25,50 +25,50 @@
       return uri + separator + key + "=" + value + hash;
     }
   }
-
-  var specs = document.querySelector('#specs');
-  specs.addEventListener('dom-change', function() {
-    schemaUrlForm = document.getElementById('schema-url-form');
-    schemaUrlInput = document.getElementById('schema-url-input');
-
-    schemaUrlForm.addEventListener('submit', function(event) {
-      event.preventDefault();
-      event.stopPropagation();
-      location.search = updateQueryStringParameter(location.search, 'url', schemaUrlInput.value)
-      return false;
-    })
-
-    schemaUrlInput.addEventListener('mousedown', function(e) {
-      e.stopPropagation();
-    });
-    schemaUrlInput.value = url;
-    specs.specs = [
-      'https://api.apis.guru/v2/specs/instagram.com/1.0.0/swagger.yaml',
-      'https://api.apis.guru/v2/specs/googleapis.com/calendar/v3/swagger.yaml',
-      'https://api.apis.guru/v2/specs/data2crm.com/1/swagger.yaml',
-      'https://api.apis.guru/v2/specs/graphhopper.com/1.0/swagger.yaml'
-    ];
-
-    var $specInput = document.getElementById('spec-input');
-
-    $specInput.addEventListener('value-changed', function(e) {
-      schemaUrlInput.value = e.detail.value;
-      location.search = updateQueryStringParameter(location.search, 'url', schemaUrlInput.value);
-    });
-
-    function selectItem() {
-      let value = this.innerText.trim();
-      schemaUrlInput.value = value;
-      location.search = updateQueryStringParameter(location.search, 'url', schemaUrlInput.value);
-    }
-
-    // for some reason events are not triggered so have to dirty fix this
-    $specInput.addEventListener('click', function(event) {
-      let $elems = document.querySelectorAll('.item.vaadin-combo-box-overlay');
-      $elems.forEach(function($el) {
-        $el.addEventListener('mousedown', selectItem);
-        $el.addEventListener('mousedown', selectItem);
-      });
-    });
-  });
+  //
+  // var specs = document.querySelector('#specs');
+  // specs.addEventListener('dom-change', function() {
+  //   schemaUrlForm = document.getElementById('schema-url-form');
+  //   schemaUrlInput = document.getElementById('schema-url-input');
+  //
+  //   schemaUrlForm.addEventListener('submit', function(event) {
+  //     event.preventDefault();
+  //     event.stopPropagation();
+  //     location.search = updateQueryStringParameter(location.search, 'url', schemaUrlInput.value)
+  //     return false;
+  //   })
+  //
+  //   schemaUrlInput.addEventListener('mousedown', function(e) {
+  //     e.stopPropagation();
+  //   });
+  //   schemaUrlInput.value = url;
+  //   specs.specs = [
+  //     'https://api.apis.guru/v2/specs/instagram.com/1.0.0/swagger.yaml',
+  //     'https://api.apis.guru/v2/specs/googleapis.com/calendar/v3/swagger.yaml',
+  //     'https://api.apis.guru/v2/specs/data2crm.com/1/swagger.yaml',
+  //     'https://api.apis.guru/v2/specs/graphhopper.com/1.0/swagger.yaml'
+  //   ];
+  //
+  //   var $specInput = document.getElementById('spec-input');
+  //
+  //   $specInput.addEventListener('value-changed', function(e) {
+  //     schemaUrlInput.value = e.detail.value;
+  //     location.search = updateQueryStringParameter(location.search, 'url', schemaUrlInput.value);
+  //   });
+  //
+  //   function selectItem() {
+  //     let value = this.innerText.trim();
+  //     schemaUrlInput.value = value;
+  //     location.search = updateQueryStringParameter(location.search, 'url', schemaUrlInput.value);
+  //   }
+  //
+  //   // for some reason events are not triggered so have to dirty fix this
+  //   $specInput.addEventListener('click', function(event) {
+  //     let $elems = document.querySelectorAll('.item.vaadin-combo-box-overlay');
+  //     $elems.forEach(function($el) {
+  //       $el.addEventListener('mousedown', selectItem);
+  //       $el.addEventListener('mousedown', selectItem);
+  //     });
+  //   });
+  // });
 })();
