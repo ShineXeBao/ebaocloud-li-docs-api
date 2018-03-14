bf65207b7483e9d5b40c6fb430efdf5f3d18d663
Zhu Liliang <liliang.zhu@gmail.com>
2018-03-14 09:19:48 +0800

    更新文档

--- swagger/build-swagger.sh
+++ swagger/build-swagger.sh
@@ -1,11 +1,14 @@
 # curl -o proposal.json http://106.14.50.232/proposal/v2/api-docs?group=proposals;
 # curl -o product.json http://106.14.50.232/pd/v2/api-docs?group=products;
 
+#文件合并和初始化
 node merge.js -p $1
 
+#抽取翻译字符串
 node i18n.js -p $1
 
+#生成最终文件
 node translation.js -p $1 -l en
-# node translation.js -p $1 -l cn
 
+#拷贝到对应目录
 cp  ./projects/$1/dist/swagger*.json ../dist/swagger/
